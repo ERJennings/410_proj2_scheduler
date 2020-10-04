6fd9ecba2219ff54983353eaded179456e2f993b
ERJennings <erjennings13@gmail.com>
2020-10-03 18:11:51 -0700

    I PUT A ZERO INSTEAD OF A 1 AND IT TOOK ME LIKE TWO HOURS TO FIND THE ERROR

--- scheduler/scheduler_SRTF.cpp
+++ scheduler/scheduler_SRTF.cpp
@@ -13,6 +13,7 @@
 #include "../includes/scheduler_SRTF.h"
 
 bool Scheduler_SRTF::time_to_switch_processes(int tick_count, PCB &p) {
+	sort();
 	int remainingTime = p.remaining_cpu_time;
 	if (remainingTime > 0 && remainingTime > ready_q->front().remaining_cpu_time) {
 		return true;
@@ -24,17 +25,18 @@ bool Scheduler_SRTF::time_to_switch_processes(int tick_count, PCB &p) {
 }
 
 void Scheduler_SRTF::sort() {
+	PCB higher;
 	std::vector<PCB> sortingVector;
 
-	for (int i =0; i< ready_q->size(); i++){
+	for (int i = 1; i < ready_q->size(); i++){
 		sortingVector.push_back(ready_q->front());
 		ready_q->pop();
 	}
 
+
 	// Not efficient according to CPSC 420 but it works.
-	PCB higher;
 	for (int i = 0; i < sortingVector.size(); i++) {
-		for (int j = 0; j < sortingVector.size(); j++) {
+		for (int j = i + 1; j < sortingVector.size(); j++) {
 			if (sortingVector[i].remaining_cpu_time > sortingVector[j].remaining_cpu_time) {
 				higher = sortingVector[i];
 				sortingVector[i] = sortingVector[j];
