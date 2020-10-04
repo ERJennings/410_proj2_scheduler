b244d0a3a3216efd0fefd06e9690e6caa4a5ee5e
ERJennings <erjennings13@gmail.com>
2020-10-03 18:58:12 -0700

    0 tests failed, code needs cleanup and style

--- scheduler/scheduler_RR.cpp
+++ scheduler/scheduler_RR.cpp
@@ -14,8 +14,19 @@
 
 bool  Scheduler_RR::time_to_switch_processes(int tick_count, PCB &p) {
 
-	bool change = Scheduler::time_to_switch_processes(tick_count, p);
-	return change;
+	//bool change = Scheduler::time_to_switch_processes(tick_count, p);
+	//return change;
+
+	int remaining = p.remaining_cpu_time;
+	int required = p.required_cpu_time;
+	if (remaining > 0 && preemptive && (required - remaining) % time_slice ==0) {
+		return true;
+	}
+	else if (remaining <= 0) {
+		return true;
+	}
+	return false;
+
 }
 
 void Scheduler_RR::sort() {
